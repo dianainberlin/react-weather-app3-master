ab0292fcbe2faef35f6de10a91f49d79ba7fac95
dianainberlin <145753245+dianainberlin@users.noreply.github.com>
2024-01-26 12:12:26 +0100

    created Weather.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
+import Weather from "./Weather";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Weather />
   </React.StrictMode>
 );
