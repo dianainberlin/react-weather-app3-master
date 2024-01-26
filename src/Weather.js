d3f9e22e9b80d84890f15739c712bfdd60e14123
dianainberlin <145753245+dianainberlin@users.noreply.github.com>
2024-01-26 13:36:27 +0100

    added unit

--- src/Weather.js
+++ src/Weather.js
@@ -2,37 +2,39 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div class="container">
-      <form class="d-flex" role="search">
+    <div className="container">
+      <form className="d-flex" role="search">
         <input
-          class="form-control me-2"
+          className="form-control me-2"
           type="search"
           placeholder="Search city"
           aria-label="Search"
           autoFocus="on"
         />
-        <button class="btn" type="submit">
+        <button className="btn" type="submit">
           Search
         </button>
       </form>
       <br />
-      <div class="row boxes">
-        <div class="col-md main-weather-box rounded-4 p-5">
-          <div class="row">
-            <div class="col">
+      <div className="row boxes">
+        <div className="col-md main-weather-box rounded-4 p-5">
+          <div className="row">
+            <div className="col">
               <ul>
                 <li>
                   <h4>Manila</h4>
                 </li>
                 <li>
-                  <h1>12°</h1>
+                  <h1>
+                    12<span className="unit">°C</span>
+                  </h1>
                 </li>
                 <li>
                   <p>Partly cloudy</p>
                 </li>
               </ul>
             </div>
-            <div class="col">
+            <div className="col">
               <img
                 src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
                 alt="cloud"
@@ -41,17 +43,17 @@ export default function Weather() {
             </div>
           </div>
         </div>
-        <div class="col-md">
-          <div class="row boxes-1">
-            <div class="col humidity-box rounded-4 p-5">
+        <div className="col-md">
+          <div className="row boxes-1">
+            <div className="col humidity-box rounded-4 p-5">
               <ul>
                 <li>Feels 10°C</li>
                 <li>H: 93% | W: 3km/h</li>
               </ul>
             </div>
           </div>
-          <div class="row">
-            <div class="col date-box gy-2 rounded-4 p-5">
+          <div className="row">
+            <div className="col date-box gy-2 rounded-4 p-5">
               <ul>
                 <li>7:00pm</li>
                 <li>28 October, Saturday</li>
