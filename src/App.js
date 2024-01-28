2af244d384eebf1e2de96e5c22ef53b5504ddd98
dianainberlin <145753245+dianainberlin@users.noreply.github.com>
2024-01-28 22:55:38 +0100

    updated city

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
           </a>
         </div>
       </nav>
-      <Weather defaultCity="Berlin" />
+      <Weather defaultCity="Manila" />
       <footer>
         Code by{" "}
         <a
